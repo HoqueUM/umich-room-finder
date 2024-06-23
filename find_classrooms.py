a769bccdebe19187748e7fa59d186cbae554c105
Rahul Hoque <rhoque@umich.edu>
2024-06-23 18:40:12 -0400

    made most of the logic

--- find_classrooms.py
+++ find_classrooms.py
@@ -3,33 +3,53 @@ import json
 from test import json_maker
 import datetime
 
-token_generator('classrooms')
-
 def get_meeting_times(room, start_date, end_date):
     """
     Fetch meeting times for a given room and date range.
     """
+    token_generator('classrooms')
     times = json_maker('classrooms', room=room, start_date=start_date, end_date=end_date)
     return times['Classrooms']['Classroom']
 
-def main():
+def get_intervals():
     with open('classrooms.json', 'r') as file:
         data = json.load(file)
-
     classes = data['Classrooms']['Classroom']
     today = datetime.date.today().strftime('%B %d, %Y')
-
+    march20 = '03-20-2024'
+    schedule_dict = {}
+    limiter = 0
     for classroom in classes:
         facility_id = classroom['FacilityID']
-        print(f"Meeting times for {facility_id} on {today}:")
-        
-        times = get_meeting_times(facility_id, today, today)
-        
+        times = get_meeting_times(facility_id, march20, march20)
+        time_intervals = []
         for meeting in times:
             start_time = meeting['MtgStartTime']
             end_time = meeting['MtgEndTime']
-            print(f"Start: {start_time}, End: {end_time}")
-        print("-" * 40)  
+            time_intervals.append((start_time, end_time))
+        schedule_dict[facility_id] = time_intervals
+        limiter += 1
+        if limiter == 5:
+            break
+    return schedule_dict
 
-if __name__ == "__main__":
-    main()
+def is_available(start_time, end_time):
+    start_time = datetime.strptime(start_time, "%H:%M")
+    end_time = datetime.strptime(end_time, "%H:%M")
+    
+    now = datetime.now().time()
+    
+    if start_time.time() <= now <= end_time.time():
+        return True
+    return False
+
+def get_available_times():
+    times = get_intervals()
+    available_times = {}
+    for room, intervals in times.items():
+        available_times[room] = []
+        for interval in intervals:
+            start_time, end_time = interval
+            if is_available(start_time, end_time):
+                available_times[room].append((start_time, end_time))
+    return available_times
---
+++ main.py
@@ -0,0 +1,8 @@
+from find_classrooms import get_intervals, get_available_times
+
+
+
+times = get_intervals()
+print(times)
+available = get_available_times()
+print(available)
